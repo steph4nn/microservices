e02ac992897ce6c42966c312af37852c7f6256f9
step4hnn <laurostephan@gmail.com>
2026-01-24 18:40:22 -0300

    parte 4

--- order/go.mod
+++ order/go.mod
@@ -11,6 +11,7 @@ require (
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/net v0.47.0 // indirect
--- order/internal/adapters/payment/payment.go
+++ order/internal/adapters/payment/payment.go
@@ -3,11 +3,15 @@ package payment
 import (
 	"context"
 	"log"
+	"time"
 
 	pb "github.com/ruandg/microservices-proto/golang/payment"
 	"github.com/ruandg/microservices/order/internal/application/core/domain"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
 )
 
 type Adapter struct {
@@ -16,9 +20,15 @@ type Adapter struct {
 
 func NewAdapter(paymentServiceUrl string) (*Adapter, error) {
 	var opts []grpc.DialOption
+	opts = append(opts,
+		grpc.WithUnaryInterceptor(grpc_retry.UnaryClientInterceptor(
+			grpc_retry.WithCodes(codes.Unavailable, codes.ResourceExhausted),
+			grpc_retry.WithMax(5),
+			grpc_retry.WithBackoff(grpc_retry.BackoffLinear(time.Second)),
+		)))
 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	conn, err := grpc.Dial(paymentServiceUrl, opts...)
-	if err != nil {
+	if err != nil {		
 		log.Fatalf("Failed to create a new adapter adapter.Error: %v", err)
 		return nil, err
 	}
@@ -27,10 +37,18 @@ func NewAdapter(paymentServiceUrl string) (*Adapter, error) {
 }
 
 func (a *Adapter) Charge(order domain.Order) error {
-	_, err := a.payment.Create(context.Background(), &pb.CreatePaymentRequest{
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	
+	_, err := a.payment.Create(ctx, &pb.CreatePaymentRequest{
 		UserId:     order.CustomerID,
 		OrderId:   order.ID,
 		TotalPrice: order.TotalPrice(),
 	})
+	
+	if status.Code(err) == codes.DeadlineExceeded {
+		log.Printf("Payment request exceeded deadline for order ID %d", order.ID)
+	}
+	
 	return err
 }
